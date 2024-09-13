25a10a722e5e230779741b0437852847e02971e0
lixxie29 <lixxxieee@gmail.com>
2024-09-13 14:19:38 +0300

    2:19pm rest api trial

---
+++ payroll/payroll/src/main/java/com/example/payroll/database/LoadDB.java
@@ -0,0 +1,25 @@
+package com.example.payroll.database;
+
+import com.example.payroll.domain.Employee;
+import com.example.payroll.repository.EmployeeRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDB {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDB.class);
+    //spring runs all command runner beans once app context is loaded, it requests a copy of the repo
+    //it creates two entities and stores them
+
+    @Bean
+    CommandLineRunner initDB(EmployeeRepo repo){
+        return args -> {
+            log.info("preload" + repo.save(new Employee("John Doe", "proffesor")));
+            log.info("preload" + repo.save(new Employee("Ale Xandra", "director")));
+        };
+    }
+}
